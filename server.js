1150caaf0787bf4f1bb4a7bcd35dde428fb0ddc2
TCMooney <tcmooney88@gmail.com>
2020-11-17 15:59:28 -0500

    route testing

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/blogPost', require('./routes/api/blogPost'));
 
-const port = process.env.PORT || 4000;
+app.get('/', (req, res) => {
+    res.send("Hello World")
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
