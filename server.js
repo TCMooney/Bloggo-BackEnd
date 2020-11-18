88bc79870e6b57f7dbf2ae6c45e352191ebedd8e
TCMooney <tcmooney88@gmail.com>
2020-11-18 13:05:39 -0500

    space

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.get('/', (req, res) => {
     res.send("Hello World")
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 2000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
