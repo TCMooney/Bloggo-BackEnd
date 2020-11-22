e60e6ccc308555b58f3946091bfde80a21b297e5
TCMooney <tcmooney88@gmail.com>
2020-11-22 13:31:00 -0500

    httpOnly: true

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -34,8 +34,8 @@ router.post('/', (req, res) => {
                         (err, token) => {
                             if (err) throw err;
                             res.cookie('access_token', token, {
-                                maxAge: 2 * 60 * 60 * 1000
-                                // httpOnly: true
+                                maxAge: 2 * 60 * 60 * 1000,
+                                httpOnly: true
                                 // secure: true
                             })
                             res.json({
